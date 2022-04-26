1ff8326a93ab2bc38edfd043d524e803326c9976
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-04-27 01:00:58 +0900

    プリミティブ型の変換

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
-"use strict";
-const message = "Hello, TypeScript!";
-console.log(message);
+import { createInterface } from 'readline';
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('文字列を入力して下さい:', (line) => {
+    console.log(`${line}が入力されました`);
+    rl.close();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-const message:string = "Hello, TypeScript!";
+import {createInterface} from 'readline';
 
-console.log(message);
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('文字列を入力して下さい:',(line) =>{
+    console.log(`${line}が入力されました`);
+    rl.close();
+})
