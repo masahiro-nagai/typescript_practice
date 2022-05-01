f7dfbf916b55fb63a0d718e93ec0b38efa3bf10a
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-02 00:05:34 +0900

    FizzBuzz

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { createInterface } from "readline";
-
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('名前を入力してください:',(name) => {
-    console.log("こんにちは、" + name + "さん");
-})
+for(let i=1; i<=100;i++){
+    if(i%3 ===0 && i%5 ===0){
+        console.log("FizzBuzz");
+    }else if(i%3 ===0){
+        console.log("Fizz");
+    }else if(i%5 === 0){
+        console.log("Buzz")
+    }
+    else{
+       console.log(i);
+    }
+}
